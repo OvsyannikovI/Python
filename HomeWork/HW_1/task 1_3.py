bdc7d0a5bde029033ba782bb2f3c4f63e6fad06d
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-09-25 13:25:11 +0300

    HW 1.4

--- HomeWork/HW_1/task 1_3.py
+++ HomeWork/HW_1/task 1_3.py
@@ -8,14 +8,16 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
-x = int(input("Enter x"))
-y = int(input("Enter y"))
+x = int(input("Enter x "))
+y = int(input("Enter y "))
 
-if x >= 0 and y >= 0:
+if x > 0 and y > 0:
     print("-> 1")
-elif x <= 0 and y >= 0:
+elif x < 0 and y > 0:
     print("-> 2")
-elif x <= 0 and y <= 0:
+elif x < 0 and y < 0:
     print("-> 3")
-elif x >= 0 and y <= 0:
+elif x > 0 and y < 0:
     print("-> 4")
+elif x == 0 or y == 0:
+    print("X ≠ 0 и Y ≠ 0")
