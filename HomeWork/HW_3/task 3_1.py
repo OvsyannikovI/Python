5e6e682f729720ae8e3690c6f64ae9db433d63c0
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-10-02 18:47:45 +0300

    HW_3 task 3_1

--- HomeWork/HW_3/task 3_1.py
+++ HomeWork/HW_3/task 3_1.py
@@ -10,5 +10,20 @@
 # out
 # [10, 2, 3, 8, 9]
 # 22
+import random
 
-count = int(input("Введите "))
+count = int(input("Введите количество чисел списка: "))
+
+def createList(num):
+    my_list = []
+    for i in range(count):
+        my_list.append(round(random.random()*10))
+    return my_list
+
+arr = createList(count)
+print(arr)
+sum = 0
+for i in range(0, len(arr)):
+    if (i+1)%2 == 0:
+        sum += int(arr[i])
+print(sum)
