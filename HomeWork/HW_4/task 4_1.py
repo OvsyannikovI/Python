eee7b93d717789d8c511f1f4b859fdaff34f962f
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-10-06 06:00:10 +0300

    task 4_1

--- HomeWork/HW_4/task 4_1.py
+++ HomeWork/HW_4/task 4_1.py
@@ -3,13 +3,15 @@
 # in
 # Enter a real number: 9
 # Enter the required accuracy '0.0001': 0.000001
-
 # out
 # 9.000000
-
 # in
 # Enter a real number: 8.98785
 # Enter the required accuracy '0.0001': 0.001
-
 # out
 # 8.988
+
+numberInput = int(input("Enter a real number: "))
+reqAcc = int(input("Enter the required accuracy: '0.0001'"))
+
+def 
