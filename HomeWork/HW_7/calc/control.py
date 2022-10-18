fb525685f418ec37251444983b262a4ac42104e2
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-10-18 21:24:52 +0300

    калькулятор

--- HomeWork/HW_7/calc/control.py
+++ HomeWork/HW_7/calc/control.py
@@ -1,12 +1,19 @@
 import operations as op
+import logger as log
 import view
 
 def button_click():
     act = view.get_value()
     if act == 6:
+        log.calc_logger_mes("exit")
         exit()
-    elif act in op.dict_act.keys():
-        res = op.actions[op.dict_act[act]](view.get_value(),view.get_value())
+    elif str(act) in op.dict_act:
+        a = view.get_value()
+        b = view.get_value()
+        res = op.actions[op.dict_act.get(str(act))](a,b)
+        log.calc_logger(op.dict_act.get(str(act)), a, b, res)
+        print(res)
     else:
         print("Error")
-    # view.view_data(res)
+        log.calc_logger_mes("Error")
+    
